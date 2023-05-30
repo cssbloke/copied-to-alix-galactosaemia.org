50d00d7040a680adda92ab7d069ff900fe9a8339
Neil <46479126+cssbloke@users.noreply.github.com>
2023-05-30 21:04:30 +0000

    added favicons

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Index({ allPosts: { edges }, preview }) {
   return (
     <Layout preview={preview}>
       <Head>
-        <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
+        <title>{`Galactosaemia.org`}</title>
       </Head>
       <PrimaryNav />
       <Container>
